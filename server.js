6a6da40ad6de22da837438a3340e460d144fb853
Spencer <spencerbhans@gmail.com>
2020-06-28 21:42:55 -0600

    Node.js is receiving json objects from my webpage via post request. It was a long battle trying to get this working... finally!

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const path = require('path');
+const url = require('url');
+const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use('/static', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get(/blog/, function(req, res) {
+app.get('/blog', function(req, res) {
   res.sendFile(path.join(__dirname, '/blog.html'));
 });
 
-app.get(/categories/, function(req, res) {
+app.get('/categories', function(req, res) {
   res.sendFile(path.join(__dirname, '/categories.html'));
 });
 
-app.get(/goals/, function(req, res) {
+app.get('/goals', function(req, res) {
   res.sendFile(path.join(__dirname, '/goals.html'));
 });
+
+app.post('/blog/entry', function(req, res) {
+  console.log(req.body);
+  // var title = req.body.title;
+  // var creator = req.body.creator;
+  // var tArea = req.body.tArea;
+  // console.log(title + " " + creator + " " + tArea);
+  res.sendFile(path.join(__dirname, '/entry.html'));
+})
 
 // app.get('/index.html', function(req, res) {
 //   res.sendFile(path.join(__dirname, '/index.html'));
